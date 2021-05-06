432c655eab5dcb1cf6be2583b852efe04cf4b09b
pinguh9 <pinguh@naver.com>
2021-05-06 11:58:59 +0900

    Add files via upload

---
+++ CameraManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    private static CameraManager instance;
+
+    public static CameraManager Instance
+    {
+        get
+        {
+            if (instance == null) instance = FindObjectOfType<CameraManager>();
+            return instance;
+        }
+    }
+
+    [SerializeField] private GameObject FollowCam;
+    [SerializeField] private GameObject SheriffCam;
+    [SerializeField] private GameObject ShopCam;
+
+
+    public void SetActiveFollowCam(bool active)
+    {
+        FollowCam.SetActive(active);
+    }
+
+    public void SetActiveSheriffCam(bool active)
+    {
+        SheriffCam.SetActive(active);
+    }
+
+    public void SetActiveShopCam(bool active)
+    {
+        ShopCam.SetActive(active);
+    }
+}
