b67485fb8ac358bb16a15aed3616f4356c4043d1
pinguh9 <pinguh@naver.com>
2021-05-06 11:45:18 +0900

    Add files via upload

---
+++ AmmoPack.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class AmmoPack : MonoBehaviour, IItem
+{
+    public int ammo = 30;
+
+    public void Use(GameObject target)
+    {
+        var playershooter = target.GetComponent<PlayerShooter>();
+       
+        if (playershooter != null && playershooter.gun != null) {//총을 쥐고 있으면
+            playershooter.gun.ammoRemain += ammo;
+        }
+
+        Destroy(gameObject);//자기자신을 파괴
+    }
+}
