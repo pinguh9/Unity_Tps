9818b76b17c6e14512622644fb6aae9759f55b0f
pinguh9 <pinguh@naver.com>
2021-05-06 11:48:16 +0900

    Update and rename LivingEntity.cs to LivingEntities/LivingEntity.cs

--- LivingEntities/LivingEntity.cs
+++ LivingEntities/LivingEntity.cs
@@ -5,12 +5,12 @@ public class LivingEntity : MonoBehaviour, IDamageable
 {
     public float startingHealth = 100f;//초기체력
     public float health { get; protected set; }
-    public bool dead { get; protected set; }//외부에서 값을 읽을 순  있지만 덮어쓸 수는 없음
+    public bool dead { get; protected set; }
     
-    public event Action OnDeath;//LinvingEntity가 사망하는 순간에 실행될 콜백을 외부에서 접근해서 할당할 수 있는 이벤트. 
+    public event Action OnDeath;
     
-    private const float minTimeBetDamaged = 0.1f;//공격과 공격 사이 최소 대기시간 값이 너무 크면 정상적인 공격도 막히게됨.
-    protected float lastDamagedTime;//짧은 시간 안에 공격이 두번 들어가는 예외사항이 가끔 생김
+    private const float minTimeBetDamaged = 0.1f;
+    protected float lastDamagedTime;
 
     protected bool IsInvulnerable
     {
@@ -22,7 +22,7 @@ public class LivingEntity : MonoBehaviour, IDamageable
         }
     }
     
-    protected virtual void OnEnable()//자식 클래스에서 기존 기능에 확장해서 사용해야 하기 때문에 거의 가상함수 
+    protected virtual void OnEnable()
     {
         dead = false;
         health = startingHealth;
