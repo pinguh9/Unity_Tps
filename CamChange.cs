e38559d05ed8e02fa813c46bc60f2708822542e2
pinguh9 <pinguh@naver.com>
2021-05-06 12:03:07 +0900

    Add files via upload

---
+++ CamChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamChange : MonoBehaviour
+{
+    public GameObject[] cameras;
+
+    private void Start()
+    {
+        StartCoroutine(changeCamera());
+    }
+
+    private IEnumerator changeCamera()
+    {
+        foreach (var cam in cameras)
+        {
+            cam.SetActive(true);
+            yield return new WaitForSeconds(5f);
+            cam.SetActive(false);
+        }
+        StartCoroutine(changeCamera());
+    }
+
+}
